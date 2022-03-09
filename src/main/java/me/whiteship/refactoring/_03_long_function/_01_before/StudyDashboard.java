2ea4335282ab1b261a07c7a57e07efc69a39625d
uram <gzgzg2@naver.com>
2022-03-09 17:15:01 +0900

    [refactor] 11th commit - 코딩으로 학습하는 리팩토링 - 3강 긴 함수 복습
    - Replace Temp With Query
    - Introduce Parameter Object
    - Preserve Whole Object
    - Replace Function with Command

--- src/main/java/me/whiteship/refactoring/_03_long_function/_01_before/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_01_before/StudyDashboard.java
@@ -5,10 +5,7 @@ import org.kohsuke.github.GHIssueComment;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
@@ -17,8 +14,14 @@ import java.util.concurrent.Executors;
 
 public class StudyDashboard {
 
+    private int totalNumberOfEvents;
+
+    public StudyDashboard(int totalNumberOfEvents) {
+        this.totalNumberOfEvents = totalNumberOfEvents;
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        StudyDashboard studyDashboard = new StudyDashboard();
+        StudyDashboard studyDashboard = new StudyDashboard(15);
         studyDashboard.print();
     }
 
@@ -28,10 +31,9 @@ public class StudyDashboard {
 
         ExecutorService service = Executors.newFixedThreadPool(8);
 
-        int totalNumberOfEvents = 15;
-        CountDownLatch latch = getCountDownLatch(totalNumberOfEvents);
+        CountDownLatch latch = getCountDownLatch();
 
-        for (int index = 1 ; index <= totalNumberOfEvents ; index++) {
+        for (int index = 1 ; index <= this.totalNumberOfEvents ; index++) {
             int eventId = index;
             service.execute(new Runnable() {
                 @Override
@@ -41,16 +43,7 @@ public class StudyDashboard {
                         List<GHIssueComment> comments = issue.getComments();
 
                         for (GHIssueComment comment : comments) {
-                            String username = comment.getUserName();
-                            boolean isNewUser = participants.stream().noneMatch(p -> p.username().equals(username));
-                            Participant participant = null;
-                            if (isNewUser) {
-                                participant = new Participant(username);
-                                participants.add(participant);
-                            } else {
-                                participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-                            }
-
+                            Participant participant = findParticipant(comment.getUserName(), participants);
                             participant.setHomeworkDone(eventId);
                         }
 
@@ -65,22 +58,7 @@ public class StudyDashboard {
         latch.await();
         service.shutdown();
 
-        try (FileWriter fileWriter = new FileWriter("participants.md");
-             PrintWriter writer = new PrintWriter(fileWriter)) {
-            participants.sort(Comparator.comparing(Participant::username));
-
-            writer.print(header(totalNumberOfEvents, participants.size()));
-
-            participants.forEach(p -> {
-                String markdownForHomework = getMarkdownForParticipant(totalNumberOfEvents, p);
-                writer.print(markdownForHomework);
-            });
-        }
-    }
-
-    private CountDownLatch getCountDownLatch(int totalNumberOfEvents) {
-        CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
-        return latch;
+        new StudyPrinter(participants, this.totalNumberOfEvents).execute();
     }
 
     private GHRepository getGhRepository() throws IOException {
@@ -89,49 +67,21 @@ public class StudyDashboard {
         return repository;
     }
 
-    private double getRate(int totalNumberOfEvents, Participant p) {
-        long count = p.homework().values().stream()
-                .filter(v -> v == true)
-                .count();
-        double rate = count * 100 / totalNumberOfEvents;
-        return rate;
+    private CountDownLatch getCountDownLatch() {
+        CountDownLatch latch = new CountDownLatch(this.totalNumberOfEvents);
+        return latch;
     }
 
-    private String getMarkdownForParticipant(int totalNumberOfEvents, Participant p) {
-        return String.format("| %s %s | %.2f%% |\n", p.username(),
-                checkMark(p, totalNumberOfEvents), getRate(totalNumberOfEvents, p));
-    }
-
-    /**
-     * | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 |
-     * | --- | --- | --- | --- | --- |
-     */
-    private String header(int totalEvents, int totalNumberOfParticipants) {
-        StringBuilder header = new StringBuilder(String.format("| 참여자 (%d) |", totalNumberOfParticipants));
-
-        for (int index = 1; index <= totalEvents; index++) {
-            header.append(String.format(" %d주차 |", index));
+    private Participant findParticipant(String username, List<Participant> participants) {
+        boolean isNewUser = participants.stream().noneMatch(p -> p.username().equals(username));
+        Participant participant = null;
+        if (isNewUser) {
+            participant = new Participant(username);
+            participants.add(participant);
+        } else {
+            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
         }
-        header.append(" 참석율 |\n");
-
-        header.append("| --- ".repeat(Math.max(0, totalEvents + 2)));
-        header.append("|\n");
-
-        return header.toString();
+        return participant;
     }
 
-    /**
-     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
-     */
-    private String checkMark(Participant p, int totalEvents) {
-        StringBuilder line = new StringBuilder();
-        for (int i = 1 ; i <= totalEvents ; i++) {
-            if(p.homework().containsKey(i) && p.homework().get(i)) {
-                line.append("|:white_check_mark:");
-            } else {
-                line.append("|:x:");
-            }
-        }
-        return line.toString();
-    }
 }
